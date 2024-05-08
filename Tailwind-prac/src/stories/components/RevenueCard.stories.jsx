927c9e7ee14b5cd2c21187061230af866ded65d2
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-05-09 03:08:58 +0530

    storybook (uncompleted)

---
+++ Tailwind-prac/src/stories/components/RevenueCard.stories.jsx
@@ -0,0 +1,27 @@
+
+
+import { RevenueCard } from '../../components/RevenueCard';
+import { Button } from './Button';
+
+const meta = {
+  component: RevenueCard,
+} 
+
+export default meta;
+
+
+export const BigAmount= {
+ args: {
+    ordercount:"123",
+    title:"hsdg",
+    amount:"1000000"
+ }
+};
+
+export const smallAmount= {
+    args: {
+       ordercount:"123",
+       title:"hsdg",
+       amount:"100"
+    }
+   };
